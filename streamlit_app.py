d97e575a5614ae770812004c956646d1400d1aaf
roydipti <diptiroy@eaton.com>
2024-08-22 09:27:32 -0400

    Update streamlit_app.py
    adding new column

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,9 @@ st.write('The name on your Smoothie will be :',name_on_order)
 
 
 session = cnx.session()
-my_dataframe =session.table("Smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
+my_dataframe =session.table("Smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients:'
      , my_dataframe
